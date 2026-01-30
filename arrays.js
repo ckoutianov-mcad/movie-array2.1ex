09d7dfa97c72bf9f26a209f2a4ccab1a9ee437c0
Your Name <ckoutianov@mcad.edu>
2026-01-29 18:46:27 -0800

    window.alert created

--- arrays.js
+++ arrays.js
@@ -1,5 +1,11 @@
 let myMovies = ['Frankenstein', 'Tron', 'Jurassic World'];
 //myMovies.push('Nosferatu');
-console.log(myMovies);
-const yourMovie = window.prompt ("What is your Favorite movie?");
-console.log(yourMovie);
+// console.log(myMovies);
+// const yourMovie = window.prompt ("What is your Favorite movie?");
+// console.log(yourMovie);
+const userInput = window.prompt ("What is the best movie of all time?");
+myMovies.push(userInput);
+const userMovie = userInput;
+window.alert ("You chose " + userMovie);
+console.log(myMovies)
+
