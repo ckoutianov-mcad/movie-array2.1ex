c60fcf8f8281fe9f52ebeb03a414c6bad7bcdf5f
Your Name <ckoutianov@mcad.edu>
2026-01-29 20:41:09 -0800

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,11 +1,13 @@
-let myMovies = ['Frankenstein', 'Tron', 'Jurassic World', 'Nosferatu', 'Sketch'];
+let myMovies = ['Frankenstein' , 'Tron' , 'Jurassic World' , 'Nosferatu' , 'Sketch' ];
 //myMovies.push('Nosferatu');
-// console.log(myMovies);
+console.log("My recent favorite films ", myMovies);
 // const yourMovie = window.prompt ("What is your Favorite movie?");
-// console.log(yourMovie);
-const userInput = window.prompt ("What is the best movie of all time?");
+
+const userInput = window.prompt ("What is your favorite movie of all time?");
+console.log("Your favorite movie is " + userInput);
+
 const userMovie = userInput;
-window.alert ("You chose " + userMovie);
+window.alert ("You chose " + userMovie + ". I have to watch that soon!");
 myMovies.push(userMovie);
-console.log(myMovies)
+console.log("Adding your choice to my list: " + myMovies.toString());
 
